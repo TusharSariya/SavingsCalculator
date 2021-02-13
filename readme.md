cec731d3e6c82dfc120a1f821e1c95ad2d63e293
tushar Sariya <tushar.97@hotmail.com>
2021-02-13 09:15:41 -0500

    basic recursive monthly savings calculator

---
+++ readme.md
@@ -0,0 +1,4 @@
+Savings Calculator
+The idea is to make the most detailed savings calculator with all variables exposed.
+
+"I like money, and I like when my money makes money"
--- src/com/SavingsCalculator/Main.java
+++ src/com/SavingsCalculator/Main.java
@@ -5,6 +5,24 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello WRLD");
+        Savings(480);
+        System.out.println("total Savings: "+ totalSavings);
 
+    }
+
+    //static int Years = 40;
+    //static int Months = Years*12;
+    static int monthlySavings = 500;
+    static double totalSavings = 0;
+    static double yearlyInterest = 0.07;
+    static double monthlyInterest = yearlyInterest/12;
+
+    static void Savings(int Months) {
+        if(Months>0)
+        {
+            totalSavings = totalSavings*(1+monthlyInterest)+monthlySavings;
+            System.out.println("total Savings: "+ totalSavings);
+            Savings(--Months);
+        }
     }
 }
